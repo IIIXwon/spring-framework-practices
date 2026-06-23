98c490b895a2fa4a72b804ef40b861fdb4b8ee15
IIIXwon <IIIXwon@gmail.com>
2026-06-23 20:57:02 +0900

    feat: JUnit실행환경과, 스프링 싱글톤 빈 확인

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -7,43 +7,35 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringJUnitConfig
 public class JUnitTest {
     @Autowired ApplicationContext context;
-    Set<JUnitTest> testObjects;
-    ApplicationContext contextObject;
+    static Set<JUnitTest> testObjects = new HashSet<>();
+    static ApplicationContext contextObject = null;
 
-    @BeforeEach
-    void setUp() {
-        testObjects = new HashSet<>();
-        contextObject = null;
-    }
 
     @Test
     void test1() {
-        assertTrue(!testObjects.contains(this));
-        testObjects.add(this);
-        assertTrue(contextObject == null || context == contextObject);
-        contextObject = context;
+        assertAll(
+                () -> assertTrue(!testObjects.contains(this)),
+                () -> testObjects.add(this),
+                () -> assertEquals(1, testObjects.size()),
+                () -> assertTrue(contextObject == null || context == contextObject),
+                () -> contextObject = context
+        );
     }
 
     @Test
     void test2() {
-        assertTrue(!testObjects.contains(this));
-        testObjects.add(this);
-        assertTrue(contextObject == null || context == contextObject);
-        contextObject = context;
-    }
-
-    @Test
-    void test3() {
-        assertTrue(!testObjects.contains(this));
-        testObjects.add(this);
-        assertEquals(contextObject, this.contextObject);
-        contextObject = context;
+        assertAll(
+                () -> assertTrue(!testObjects.contains(this)),
+                () -> testObjects.add(this),
+                () -> assertEquals(2, testObjects.size()),
+                () -> assertTrue(contextObject == null || context == contextObject),
+                () -> contextObject = context
+        );
     }
 }
